892d5758c9a65e88970e7727743d35c7274aa510
Joan Salas <joan.salas.silva@gmail.com>
2022-06-06 15:59:14 -0400

    enviar parámetros por ruta

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,6 +5,7 @@ import AboutPage from "../pages/AboutPage";
 import ContactPage from "../pages/ContactPage";
 import HomePage from "../pages/HomePage";
 import NotFoundPage from "../pages/NotFoundPage";
+import ProfilePage from "../pages/ProfilePage";
 
 export const AppRouter = () => {
   return (
@@ -13,6 +14,8 @@ export const AppRouter = () => {
       <Routes>
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+
         <Route exact path="/" element={<HomePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
